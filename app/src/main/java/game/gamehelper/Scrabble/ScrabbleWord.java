59e647ec793a5ae46f93620c2ab0f93834d20bd8
13unlucky <13unlucky777@gmail.com>
2015-04-13 15:12:28 -0400

    Added a ScrabbleWord class that has a String word and int score created with a string, it store the string in upper case and calculates the score on creation. Also created a getScrabbleWords which creates an array of Scrabble words based on an input string using a web api for anagrams

---
+++ app/src/main/java/game/gamehelper/Scrabble/ScrabbleWord.java
@@ -0,0 +1,30 @@
+package game.gamehelper.Scrabble;
+
+/**
+ * Created by Andrew on 4/13/2015.
+ */
+public class ScrabbleWord {
+
+    int score;
+    String word;
+
+    ScrabbleWord(String in)
+    {
+        word = in.toUpperCase();
+        setScore();
+    }
+
+
+    
+    private void setScore()
+    {
+        score = 0;
+        int[] scoreTable = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+
+        char[] charArray = word.toCharArray();
+        for(char c: charArray)
+        {
+            score += scoreTable[c - 'A'];
+        }
+    }
+}
---
+++ app/src/main/java/game/gamehelper/Scrabble/getScrabbleWords.java
@@ -0,0 +1,84 @@
+package game.gamehelper.Scrabble;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+/**
+ * Created by Andrew on 4/13/2015.
+ */
+
+
+public class getScrabbleWords
+{
+
+    public ScrabbleWord[] getWordArray(String  letters)
+    {
+
+        //creat the url for request
+        String urlString = new String("http://www.anagramica.com/all/:");
+        urlString += letters;
+
+        ArrayList<ScrabbleWord> wordList = new ArrayList<>();
+
+
+        try
+        {
+            URL url = new URL(urlString);
+            //connect to the url
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.connect();
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            //compile the response in to a string
+            String temp, response = "";
+            while ((temp = bReader.readLine()) != null)
+            {
+                response += temp;
+            }
+
+            //close reader and connection
+            bReader.close();
+            connection.disconnect();
+
+            //creat a JSON object from the url response
+            JSONObject jObject = new JSONObject(response);
+
+            //get the array from that object
+            JSONArray jArray = jObject.getJSONArray("all");
+
+            //add all the JSON array objects bigger then length 1 to a list
+            for (int i=0; i < jArray.length(); i++)
+            {
+                String word = (String) jArray.get(i);
+                if(word.length() > 1)
+                {
+                    ScrabbleWord sWord = new ScrabbleWord(word);
+                    wordList.add(sWord);
+                }
+            }
+
+        }
+        catch(Exception e)
+        {
+
+        }
+
+        //return the list as an array
+        return wordList.toArray(new ScrabbleWord[wordList.size()]);
+    }
+
+
+
+
+}
